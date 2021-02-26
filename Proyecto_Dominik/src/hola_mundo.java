46acd2cabd7e6c873aab15660891d61c4a7f48cd
Dominik <dominik116@hotmail.es>
2021-02-26 13:03:32 +0100

    Tercera revisión

--- Proyecto_Dominik/src/hola_mundo.java
+++ Proyecto_Dominik/src/hola_mundo.java
@@ -1,11 +1,14 @@
-
 public class hola_mundo {
 
 	public static void main(String[] args) {
+		String frase="Hola mundo";
+		boolean saludo=true;
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo.");
 		
 		System.out.println("Adiós mundo.");
+		
+		System.out.println(frase=(saludo)?"Hola":"Adios");
 	}
 
 }
