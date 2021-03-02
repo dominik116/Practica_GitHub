cffba983dafc81e38c6c1c94471a34176c867728
Dominik <dominik116@hotmail.es>
2021-03-02 15:36:16 +0100

    Segunda versión

--- Proyecto_Dominik/src/hola_mundo.java
+++ Proyecto_Dominik/src/hola_mundo.java
@@ -4,7 +4,5 @@ public class hola_mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo.");
-		
-		System.out.println("Adiós mundo.");
 	}
 }
